82580a53bc20bf5bda3be551f10a7fb9b6c4ca24
NalanStudent <215986@student.upm.edu.my>
2026-08-12 13:07:41 +0800

    chore(git): untrack rag_dev folder from git index as specified in .gitignore

--- rag_dev/streamlit_app.py
+++
@@ -1,5 +0,0 @@
-"""Main Streamlit application serving as the UI entrypoint."""
-import streamlit as st
-
-st.title("InvoiceInsight RAG Chatbot")
-st.write("Welcome! This is the proof-of-concept RAG sandbox dashboard.")
